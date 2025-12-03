6aaab52d607395b05bae108420ce433e32675f15
Admin <m_v_lazareva@mail.ru>
2025-12-03 13:07:46 +0300

    edit_5

--- src/test/java/work/part01/WorkTest.java
+++ src/test/java/work/part01/WorkTest.java
@@ -6,11 +6,11 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.sleep;
 
-public class work1 {
+public class WorkTest {
     @Test
     void test04() {
         open("https://slqa.ru/cases/SimpleForm/");
         $("body").shouldHave(text("Томас Фуллер"));
-        sleep(10000);
+        sleep(20000);
     }
 }
