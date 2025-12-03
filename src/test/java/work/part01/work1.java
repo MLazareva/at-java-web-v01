4ce8249941f8323e3037deaaeaefeed570d27774
Admin <m_v_lazareva@mail.ru>
2025-12-03 12:49:53 +0300

    edit_3

--- src/test/java/work/part01/work1.java
+++ src/test/java/work/part01/work1.java
@@ -1,12 +1,16 @@
 package work.part01;
 
+import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.sleep;
 
 public class work1 {
-    void test01() {
+    @Test
+    void test04() {
         open("https://slqa.ru/cases/SimpleForm/");
         $("body").shouldHave(text("Томас Фуллер"));
         sleep(10000);
     }
+}
