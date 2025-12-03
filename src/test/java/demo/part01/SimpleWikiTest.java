6371c5050d84a8dcde07c5b48dd2fff11689628c
Admin <m_v_lazareva@mail.ru>
2025-12-03 12:26:53 +0300

    edit_1

--- src/test/java/demo/part01/SimpleWikiTest.java
+++ src/test/java/demo/part01/SimpleWikiTest.java
@@ -2,14 +2,18 @@ package demo.part01;
 
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+
 public class SimpleWikiTest {
     @Test
     void test01() {
         open("https://ru.wikipedia.org/wiki/Selenium");
         $("body").shouldHave(text("WebDriver"));
+        sleep(10000);;
     }
+
+
+
     @Test
     void test02() {
         open("https://ru.wikipedia.org/wiki/Selenium");
