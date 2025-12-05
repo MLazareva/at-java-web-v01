0969e162ace4c8f7e69a145fa4df8c93004432b4
Admin <m_v_lazareva@mail.ru>
2025-12-05 13:46:55 +0300

    ДЗ урока XX: описание что сделано

--- src/test/java/packageTest/WorkTestTest.java
+++ src/test/java/packageTest/WorkTestTest.java
@@ -1,21 +1,36 @@
 package packageTest;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class WorkTestTest {
     @Test
     void test03() {
+        Configuration.browser = "ie";
+        Configuration.browserSize = "900x400";
+        Configuration.browserPosition = "100x500";
         open("http://92.51.36.108:7777/sl.qa ");
+
+        sleep(2000);
+        getWebDriver().manage().window().maximize();
+        /*
+     ! спросить почему не работает
+    ().manage().window().maximize();
+    */
         $("body").shouldHave(text("Учебные приложения"));
-        sleep(10000);
+        sleep(1_000);
         // sleep(20000);
     }
-        @Test
-        void test04() {
-            open("http://92.51.36.108:7777/sl.qa");
-            $("body").shouldHave(text("Промышленные приложения"));
-        }
+
+
+ //   @Test
+  //      void test04() {
+  //          open("http://92.51.36.108:7777/sl.qa");
+   //         $("body").shouldHave(text("Промышленные приложения"));
+   //     }
     }
