69a4718f0fa547604f3ee993c00d337f118a9250
Student <m_v_lazareva@mail.ru>
2025-12-07 12:24:31 +0300

    q1

--- src/test/java/demo/part01/SimpleWikiTest.java
+++ src/test/java/demo/part01/SimpleWikiTest.java
@@ -1,18 +1,32 @@
 package demo.part01;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.By.cssSelector;
+
 public class SimpleWikiTest {
+   // @Test
+   // void test01() {
+     //   open("https://ru.wikipedia.org/wiki/Selenium");
+     //   $("body").shouldHave(text("WebDriver"));
+  //  }
+
+
     @Test
-    void test01() {
-        open("https://ru.wikipedia.org/wiki/Selenium");
-        $("body").shouldHave(text("WebDriver"));
+    void test03() {
+     open("https://slqa.ru/cases/SimpleForm");
+        sleep(10000);
+        open("//ru.wiki");
+     $("body").shouldHave(text("Томас Фуллер"));
     }
-    @Test
-    void test02() {
-        open("https://ru.wikipedia.org/wiki/Selenium");
-        $("body").shouldHave(text("Selenide"));
-    }
+
+
+ //   @Test
+   // void test02() {
+    //   open("https://ru.wikipedia.org/wiki/Selenium");
+     //   $("body").shouldHave(text("Selenide"));
+  // }
 }
--- src/test/java/work/part01/test_work01.java
+++ src/test/java/work/part01/test_work01.java
@@ -1,4 +1,40 @@
 package work.part01;
 
+import com.codeborne.selenide.*;
+import com.codeborne.selenide.ex.FileNotDownloadedError;
+import com.codeborne.selenide.files.FileFilter;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Coordinates;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.time.Duration;
+import java.util.List;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
 public class test_work01 {
+    @Test
+    void test001() {
+        Configuration.browser = "firefox";
+        Configuration.browserSize = "800x400";
+        configuration.
+
+        open("http://92.51.36.108:7777/sl.qa");
+        sleep(10000);
+        SelenideElement a = $("body");
+        a.shouldHave(text("Учебные приложения"));
+        }
+       // $("body").shouldHave(text("Учебные приложения"));
+
+    @Test
+    void test002() {
+        open("http://92.51.36.108:7777/sl.qa");
+        //sleep(10000);
+        $("body").shouldHave(text("Учебные приложения"));
+    }
 }
