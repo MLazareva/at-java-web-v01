0b2d88d6df848356885fb7ec476026ecf571dddb
Student <m_v_lazareva@mail.ru>
2025-12-14 16:38:07 +0300

    lazareva_работа 80

---
+++ src/test/java/work/part04/TestParam.java
@@ -0,0 +1,29 @@
+package work.part04;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestParam {
+    @ParameterizedTest(name = "01. Проверка логинов, #{index}, username:{0}")
+    @ValueSource (strings = "standard_user","locked_out_user","problem_user");
+
+    public void test01LoginSuccess() {
+        open("https://slqamsk.github.io/cases/slflights/v01/");
+
+
+        SelenideElement myElement = $("input[name+username]");
+        $(By.id("username")).sendKeys("standard_user");
+        $(By.id("password")).type("stand_pass1");
+        $(By.id("loginButton")).click();
+        $(By.className("greeting")).shouldHave(text("Добро пожаловать, Иванов Иван Иванович!"));
+
+    }
+
+}
