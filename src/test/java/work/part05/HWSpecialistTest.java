92431623896e32af4f48e5565f455ed1c6e5abd0
MLazareva <m_v_lazareva@mail.ru>
2025-12-20 17:07:41 +0300

    Задание АТ-1

---
+++ src/test/java/work/part05/HWSpecialistTest.java
@@ -0,0 +1,68 @@
+package work.part05;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class HWSpecialistTest {
+    // На сайте https://www.specialist.ru/ в модальном окне "Наш сайт использует
+    // файлы cookie" нажать "Согласен". Выбрать пункт меню "Форматы обучения",
+    // затем "Свободное обучение", затем "Выбрать курс", в поле "Направление" выбрать
+    // "Программирование", нажать кнопку "Применить" и убедиться, что на странице
+    // есть элемент содержащий текст "Тестирование ПО".
+    static void beforeAll() {
+        Configuration.pageLoadTimeout= 30_000; // Чтобы успел открыть страницу
+        Configuration.pageLoadStrategy = "eager";  // Жадная загрузка - мы не дожидаемся подгрузки всех ресурсов
+        // Идем дальше, как только загрузился html
+    }
+    @Test
+    public void test01_HW() {
+        open("https://specialist.ru");
+        //Закрываем окно с куками
+        $x("//button[@id='cookieConsent__ok']").click();
+        System.out.println("Закрыли куки 1 ");
+
+        $x("//a[.='Форматы обучения']").click();  //тег a с таким именем один - проверили в DevTools (Ctrl+F)
+        System.out.println("Форматы обучения 2 ");
+
+        // Выбрать 'Свободное обучение'
+        $x("//a[contains(.,'Свободное обучение')]").click();
+        sleep(10_000);
+        System.out.println("Свободное обучение 3 ");
+
+        $x("//div[@class='banner-content']/a[contains(.,'Выбрать курс')]").click();
+        sleep(10_000);
+
+        System.out.println("Выбрать курс 4 ");
+
+        //Из выпадающего списка выбираем "Программирование"
+        $x("//select[@id='Filter_CategoriesDirectionFilter']").selectOptionByValue("ПРГ");
+        sleep(10_000);
+        System.out.println("Выбрали Программирование 5 ");
+
+        //Закрываем выпадающее меню консультанта
+        $x("//jdiv[@class='closeIcon__dHH_W']")
+                .shouldHave(visible)
+                .shouldHave(clickable)
+                .click();
+
+        System.out.println("Закрыли консультации 6 ");
+
+        //Нажимаем "Применить"
+        $x("//button[@id='sendBtn']").click();
+
+        System.out.println("Нажали применить 7 ");
+
+        // Проверить, что на странице есть "Тестирование ПО"
+       // $x(" //a[@class='course-link' and contains(.,'Тестирование ПО')]").shouldHave(text("Тестирование ПО"));
+        $x("//a[@class='course-link' and @href='/course/tpo']").shouldHave(text("Тестирование ПО"));
+        sleep(10_000);
+
+        System.out.println("Нашли текст 8 ");
+
+
+    }
+}
--- src/test/java/work/part05/NewParameterizedTest.java
+++ src/test/java/work/part05/NewParameterizedTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class NewParameterizedTest {
@@ -25,10 +25,33 @@ public class NewParameterizedTest {
 
     void test02_parameter_cvs(String param1, String param2) {
         open("https://slqa.ru/cases/fc/v01/");
-        $x("//input[@name='sum']").setValue(param1);
+        $x("//input[@name='sum']").setValue(param1); //Задаем сумму
         sleep(2_000);
         $x("//input[@type='submit']").click();
         sleep(2_000);
-        $x("//span[@name='com']").shouldHave(text(param2));
+        $x("//span[@name='com']").shouldHave(exactText(param2)); //проверяем комиссию
+    }
+
+    @ParameterizedTest
+    @CsvFileSource (resources = "sl_data01.csv", numLinesToSkip = 2)
+    void test01(String login, String pass, String fio) {
+        open("https://slqamsk.github.io/cases/slflights/v01/");
+       // $("#username").sendKeys(login);
+        $x("//input[@id='username']").sendKeys(login);
+
+        //$("#password").sendKeys(pass);
+        $x("//input[@id='password']").sendKeys(pass);
+
+       // $("#loginButton").click();
+        $x("//button[@id='loginButton']").click();
+
+       // $("#logoutButton").shouldBe(visible);
+        $x("//button[@id='loginButton']").shouldBe(visible);
+
+     //   $("#greeting").shouldHave(exactText("Добро пожаловать, " + fio + "!"));
+        $x("//*[@id='greeting']").shouldHave(exactText("Добро пожаловать, " + fio + "!"));
+       // $x("//*[@id='greeting']").shouldHave(text("Добро пожаловать, "));
+       // $x("//*[@id='greeting']").shouldHave(text(fio));
+       // //$("#greeting").shouldHave(text(fio));
     }
 }
